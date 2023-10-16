aaabe34a531031b02b4bae4cceb2899571b6c7c4
afifi <afifi.mohammed95@gmail.com>
2023-10-16 11:23:19 +0300

    prepare code for interface

--- script.js
+++ script.js
@@ -1,34 +1,26 @@
 let getPlayerChoice = prompt("please enter your choice:");
+let playerSelection = getPlayerChoice.toLowerCase();
+let computerSelection = getComputerChoice();
 
 function getComputerChoice() {
-let random = Math.floor(Math.random() * 3);
-if (random === 0) {
-    return "rock"
-}else if (random === 1) {
-    return "paper"
-}else {
-    return "scissors"
-};
+    let random = Math.floor(Math.random() * 3);
+    if (random === 0) {
+        return "rock";
+    } else if (random === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    };
 };
 
 function playRound(playerSelection, computerSelection) {
-if (playerSelection == computerSelection) {
-    return "This game is a draw"
-}else if ((playerSelection == "rock" && computerSelection == "scissors")
+    if (playerSelection == computerSelection) {
+        return "This game is a draw";
+    } else if ((playerSelection == "rock" && computerSelection == "scissors")
         || (playerSelection == "paper" && computerSelection == "rock")
         || (playerSelection == "scissors" && computerSelection == "paper")) {
-            return `You won! ${playerSelection} beats ${computerSelection}`
-        }else {
-            return `You lose! ${playerSelection} loses to ${computerSelection}`
-        };
+        return `You won! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        return `You lose! ${playerSelection} loses to ${computerSelection}`;
+    };
 };
-
-function game() {
-for (let counter = 0; counter < 5; counter++) {
-    console.log(playRound(playerSelection, computerSelection))
-}
-};
-
-let playerSelection = getPlayerChoice.toLowerCase();
-let computerSelection = getComputerChoice();
-game(playerSelection, computerSelection)
