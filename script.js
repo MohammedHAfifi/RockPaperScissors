c10313ee9427fe1e6f38a9bc2447277d2aaf6c4c
afifi <afifi.mohammed95@gmail.com>
2023-10-16 14:28:12 +0300

    add score limit

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let playerSelection = '';
 let playerScore = 0;
 let computerScore = 0;
+let buttons = document.querySelectorAll('.btn');
+let result = document.querySelector('#roundResult');
+let score = document.querySelector('#score');
 
 function getComputerChoice() {
     let random = Math.floor(Math.random() * 3);
@@ -15,27 +18,36 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
-        return "This game is a draw";
+        result.textContent = "This game is a draw";
     } else if ((playerSelection == "rock" && computerSelection == "scissors")
         || (playerSelection == "paper" && computerSelection == "rock")
         || (playerSelection == "scissors" && computerSelection == "paper")) {
         playerScore++
-        return `You won! ${playerSelection} beats ${computerSelection}`;
+        result.textContent = `You won! ${playerSelection} beats ${computerSelection}`;
     } else {
         computerScore++
-        return `You lose! ${playerSelection} loses to ${computerSelection}`;
+        result.textContent = `You lose! ${playerSelection} loses to ${computerSelection}`;
     };
 };
 
-let buttons = document.querySelectorAll('.btn');
-let result = document.querySelector('#roundResult');
-let score = document.querySelector('#score');
+function playGame(playRound) {
+    let computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+    if (playerScore == 5) {
+        computerScore = 0;
+        playerScore = 0;
+        result.textContent = 'You Win!';
+    } else if (computerScore == 5) {
+        computerScore = 0;
+        playerScore = 0;
+        result.textContent = 'The Computer Won </3';
+    }
+};
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         playerSelection = button.id;
-        let computerSelection = getComputerChoice();
-        result.textContent = playRound(playerSelection, computerSelection);
+        playGame(playRound);
         score.textContent = `Player Score: ${playerScore} Computer Score: ${computerScore}`;
     })
 });
