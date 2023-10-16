ebf6a2b29d633cd062343bcc0b43f02de3d09c95
afifi <afifi.mohammed95@gmail.com>
2023-10-16 13:53:45 +0300

    add scoring system

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let playerSelection = '';
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     let random = Math.floor(Math.random() * 3);
@@ -17,19 +19,23 @@ function playRound(playerSelection, computerSelection) {
     } else if ((playerSelection == "rock" && computerSelection == "scissors")
         || (playerSelection == "paper" && computerSelection == "rock")
         || (playerSelection == "scissors" && computerSelection == "paper")) {
+        playerScore++
         return `You won! ${playerSelection} beats ${computerSelection}`;
     } else {
+        computerScore++
         return `You lose! ${playerSelection} loses to ${computerSelection}`;
     };
 };
 
 let buttons = document.querySelectorAll('.btn');
 let result = document.querySelector('#roundResult');
+let score = document.querySelector('#score');
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         playerSelection = button.id;
         let computerSelection = getComputerChoice();
         result.textContent = playRound(playerSelection, computerSelection);
+        score.textContent = `Player Score: ${playerScore} Computer Score: ${computerScore}`;
     })
 });
